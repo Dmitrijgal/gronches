95ae7819f9c57a56cb4ce566a61f6d912ae63edd
Dmitrij Galusko <dmitrijgal@gmail.com>
2021-03-03 15:28:01 +0200

    Renames Template to Templates in TemplateList strut
    For the sake of logic

--- templates_list.go
+++ templates_list.go
@@ -8,16 +8,16 @@ import (
 
 // TemplatesList is structure for saving data which later will be parsed. It countains Template structures.
 type TemplatesList struct {
-	XMLName  xml.Name   `xml:"DATA"`
-	Template []Template `xml:"ROW"`
+	XMLName   xml.Name   `xml:"DATA"`
+	Templates []Template `xml:"ROW"`
 }
 
 // AppendVariables is TemplatesList method which appends variable field to every template in list.
 func (templateList *TemplatesList) AppendVariables() {
 
-	for i, t := range templateList.Template {
+	for i, t := range templateList.Templates {
 		t.AppendVariables()
-		templateList.Template[i] = t
+		templateList.Templates[i] = t
 	}
 }
 

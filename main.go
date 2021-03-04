c232697045e2489233227ae9b1f95519097bc77c
Dmitrij Galusko <dmitrijgal@gmail.com>
2021-03-04 15:07:04 +0200

    Added/changed main func
    Better asks for input file, added default file
    Choice for output

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Input file name:")
+	fmt.Println("Input file name (press Enter for default (templates.xml)):")
 
 	fileName, err := reader.ReadString('\n')
 	if err != nil {
@@ -20,9 +20,14 @@ func main() {
 	}
 	fileName = strings.Trim(fileName, " \n")
 
+	if fileName == "" {
+		fileName = "templates.xml"
+	}
+
 	file, err := os.Open(fileName)
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
 	defer file.Close()
 
@@ -33,11 +38,26 @@ func main() {
 
 	templateData.AppendVariables()
 
-	newFile, err := xml.MarshalIndent(templateData, "", " ")
+	dataMarshalled, err := xml.MarshalIndent(templateData, "", " ")
+	if err != nil {
+		fmt.Println(err)
+
+	}
+	fmt.Println("Chose output: \n 1 for Console \n 2 for new file \n 3 for Console and file")
+	choice, err := reader.ReadString('\n')
 	if err != nil {
 		fmt.Println(err)
 	}
-
-	_ = ioutil.WriteFile("result.xml", newFile, 0644)
+	choice = strings.Trim(choice, " \n")
+	fmt.Println(choice)
+	switch {
+	case choice == "1":
+		fmt.Println(string(dataMarshalled))
+	case choice == "2":
+		_ = ioutil.WriteFile("new_"+fileName, dataMarshalled, 0644)
+	case choice == "3":
+		fmt.Println(string(dataMarshalled))
+		_ = ioutil.WriteFile("new_"+fileName, dataMarshalled, 0644)
+	}
 
 }

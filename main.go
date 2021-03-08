e789b04d7d3555d22bc1740d2fb969617bb326f7
Dmitrij Galusko <dmitrijgal@gmail.com>
2021-03-08 10:41:25 +0200

    Removed in main.go: cmd args (now uses flags), file write
    File write/os.Args() and involved code removed.
    Instead of os.Args() now using flags

--- main.go
+++ main.go
@@ -2,31 +2,18 @@ package main
 
 import (
 	"encoding/xml"
+	"flag"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"strings"
 )
 
 func main() {
-	if len(os.Args) == 2 && os.Args[1] == "help" {
-		fmt.Println("Program requires 2 arguments path and output argument. \n Output arguments: \n -c \t console \n -f \t file \n -cf \t console and file")
-		os.Exit(1)
-	}
-	if len(os.Args) < 3 {
-		fmt.Println("Not enought arguments. See help.")
-		os.Exit(1)
-	}
+	filePath := flag.String("path", "templates.xml", "A path to file.")
 
-	if len(os.Args) > 3 {
-		fmt.Println("Too many arguments. See help.")
-		os.Exit(1)
-	}
+	flag.Parse()
 
-	filePath := os.Args[1]
-	outputArguments := os.Args[2]
-
-	file, err := os.Open(filePath)
+	file, err := os.Open(*filePath)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -52,16 +39,6 @@ func main() {
 		"&amp;", "&", "&lt;", "<", "&gt;", ">", "&#xD;", "\r")
 	dataMarshalled = []byte(Replacer.Replace(string(dataMarshalled)))
 
-	switch {
-	case outputArguments == "-c":
-		fmt.Println(string(dataMarshalled))
-	case outputArguments == "-f":
-		_ = ioutil.WriteFile("output.xml", dataMarshalled, 0644)
-	case outputArguments == "-fc" || outputArguments == "-cf":
-		fmt.Println(string(dataMarshalled))
-		_ = ioutil.WriteFile("output.xml", dataMarshalled, 0644)
-	default:
-		fmt.Println(outputArguments, "is not a valid comand. See help.")
-	}
+	fmt.Println(string(dataMarshalled))
 
 }
